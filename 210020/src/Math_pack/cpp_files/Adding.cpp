9b29dbb9e1ec9b32bebe9a79ece6f4def674c3b8
AayuGupt <100212842+AayuGupt@users.noreply.github.com>
2022-07-19 17:04:39 +0530

    Create Adding.cpp

---
+++ 210020/src/Math_pack/cpp_files/Adding.cpp
@@ -0,0 +1,18 @@
+#include "ros/ros.h"
+#include "math/add_two_numbers.h"
+
+bool adding(math::add_two_numbers::Request &req, math::add_two_numbers::Response &res){
+    res.sum.num = req.num1.num + req.num2.num;
+    ROS_INFO("request: x=%f, y=%f", req.num1.num, req.num2.num);
+    ROS_INFO("sending back response: [%f]", res.sum.num);
+    return true;
+}
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "add_two_numbers_server");
+    ros::NodeHandle n;
+    ros::ServiceServer service = n.advertiseService("add_two_numbers", adding);
+    ROS_INFO("Ready to add");
+    ros::spin();
+    return 0;
+} 
